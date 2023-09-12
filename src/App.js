42973103c3861c79a3157bd8e23758ac924355ac
Magen Crawford <crawford.magen@gmail.com>
2023-09-11 19:27:37 -0500

    Todo app, after refactoring code

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+import "./App.css";
+
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>React To Do App</h1>
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <TodoList list={todos} remove={deleteTodo} />
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/TodoInput.js
@@ -0,0 +1,18 @@
+const TodoInput = ({ todo, setTodo, addTodo }) => {
+    return (
+        <div className="input-wrapper">
+            <input
+                type="text"
+                name="todo"
+                value={todo}
+                placeholder="Create a new task"
+                onChange={(e) => {
+                    setTodo(e.target.value);
+                }}
+            />
+            <button className="add-button" onClick={addTodo}>Add</button>
+        </div>
+    );
+};
+
+export default TodoInput;
